7f97c8c9062339601a11f989d7025c2343ca9fe5
Donatas Adamonis <adamonis@mail.com>
2019-05-26 11:31:00 +0200

    changes

--- models/orderitems.js
+++ models/orderitems.js
@@ -3,7 +3,7 @@ const sequelize = require('../mssql');
 
 const Model = DataTypes.Model;
 
-class OrderItems extends Model {}
+class OrderItems extends Model { }
 OrderItems.init({
   OrderItemID: {
     type: DataTypes.INTEGER,
@@ -20,6 +20,10 @@ OrderItems.init({
     allowNull: false,
     defaultValue: 1
   },
+  Size: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   Price: {
     type: DataTypes.DOUBLE,
     allowNull: false
@@ -28,21 +32,33 @@ OrderItems.init({
     type: DataTypes.DOUBLE,
     allowNull: false
   },
-  ProductQuantityID: {
+  ProductID: {
     type: DataTypes.INTEGER,
     allowNull: false
   }
-}, {
-  timestamps: false,
-  hooks: {
-    afterValidate: (OrderItem, options) => {
-      const quantity = OrderItem.Quantity;
-      const oderItemID = OrderItem.OrderItemID;
-      const query = "Exec UpdateQuantity @qnt=" + quantity +", @oderItemID="+oderItemID+";";
-      return sequelize.query(query, { transaction: options.transaction});
-    }
-  },
-  sequelize
-});
+},
+  {
+    timestamps: false,
+    hooks: {
+      afterValidate: (OrderItem, options) => {
+        return sequelize.query(
+          'Exec UpdateQuantity @qnt=:Quantity, @ProductID=:ProductID, @Size=:Size',
+          { 
+            replacements: { Quantity: OrderItem.Quantity, ProductID: OrderItem.ProductID, Size: OrderItem.Size, },
+            transaction: options.transaction
+          }
+        )
+      },
+      beforeValidate: (OrderItem, options) =>{
+        return sequelize.query(`SELECT Price FROM products WHERE ProductID=:ProductID`,{
+          replacements: { ProductID: OrderItem.ProductID },
+          type: sequelize.QueryTypes.SELECT
+        }).then(result =>{
+          OrderItem.Price = result[0].Price;
+        })
+      }
+    },
+    sequelize
+  });
 
 module.exports = OrderItems;
--- queries.sql
+++ queries.sql
@@ -12,3 +12,18 @@ IF @quantity > @items
 	WHERE OrderItemID = @oderItemID
 ELSE
 	RAISERROR ('No enough products in storage', 16, 1);
+
+
+DROP PROC IF EXISTS UpdateQuantity;
+GO
+CREATE PROC UpdateQuantity (@qnt int, @oderItemID INT)
+AS
+BEGIN
+DECLARE @quantity INT = (SELECT QuantityOnStock FROM dbo.quantities WHERE ProductQuantityID = @oderItemID )
+IF @quantity > @qnt
+	UPDATE dbo.quantities
+	SET QuantityOnStock = QuantityOnStock - @qnt ,SoldQuantity = SoldQuantity + @qnt
+	WHERE ProductQuantityID = @oderItemID
+ELSE
+	RAISERROR ('No enough products in storage', 16, 1);
+END
